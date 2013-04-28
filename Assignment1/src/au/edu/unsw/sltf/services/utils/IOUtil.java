0ebfe29c308e92afb46b07b668c5feb1b581042d
nima moghadam <nmoghadam@gmail.com>
2013-04-28 11:35:55 +1000

    A utility class for IO operations.

---
+++ Assignment1/src/au/edu/unsw/sltf/services/utils/IOUtil.java
@@ -0,0 +1,39 @@
+package au.edu.unsw.sltf.services.utils;
+
+import java.io.File;
+
+import au.edu.unsw.sltf.services.bts.ext.TimeSeriesBuildingServiceExt;
+
+public class IOUtil {
+
+	public static final String timeSeriesDataFolderName = "time_series_data";
+	
+	public static final String marketDataFolderName = "market_data";
+
+	
+	public static void setupProjectDirectoryStructure(){
+		 File timeSeriesDataFolder = getTimeSeriesDataFolder();
+		 File marketDataFolder = getmarketDataFolder();
+		 
+		 if (!timeSeriesDataFolder.exists()){
+			 timeSeriesDataFolder.mkdir();
+		 }
+		 
+		 if (!marketDataFolder.exists()){
+			 marketDataFolder.mkdir();
+		 }
+	}
+	
+	public static File getTimeSeriesDataFolder(){
+		return new File(getRootFolder(), timeSeriesDataFolderName);
+	}
+	
+	public static File getmarketDataFolder(){
+		return new File(getRootFolder(), marketDataFolderName);
+	}
+	
+	public static File getRootFolder(){
+		ClassLoader classLoader = TimeSeriesBuildingServiceExt.class.getClassLoader();
+		return new File(classLoader.getResource("").getPath());
+	}
+}
